8616ad6747e3f1a1184f5a58b31cacd1a2e8872c
Raana <raanasn@yahoo.de>
2020-04-12 00:41:27 +0430

    added f_number

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
+# mishe marze predict ro taghir dad?
+# train bar asas mix f1, f1 o f2,
+# barname be barname
+# hame barname ha
+# Akhar sar bad smell ezafe
+
 import os, time
 from fit_data import test_data
 
-print(test_data(9,0.7,"weighted"))
-print(test_data(9,0.65,"weighted"))
-print(test_data(9,0.5,"weighted"))
-print(test_data(9,0.25,"weighted"))
-
+res=[]
+try:
+    #aval ino motmaen sho eyne ham
+    res.append(test_data(0, 0.7, "weighted", 0))
+    print(res)
+except:
+    os.system('mpg123 Input/fuehlen.mp3')
 os.system('mpg123 Input/ham.mp3')
