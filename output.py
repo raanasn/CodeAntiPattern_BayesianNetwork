c38c5b9859b50e7b78ab93592bcc31be56d9c64a
Raana <raanasn@yahoo.de>
2020-05-14 00:56:01 +0430

    excel color

--- output.py
+++ output.py
@@ -1,9 +1,10 @@
 import openpyxl
+import xlwings as xw
 
 def excel(name,f_number, diff_percent,split,len_bs,res,sheet):
     wb = openpyxl.load_workbook("Input/"+name+'.xlsx', read_only=False)
     ws=wb[sheet]
-    row_count = ws.max_row + 1
+    row_count = ws.max_row+1
     for i in range(split):
         r = row_count + i
         ws.cell(r, 1).value = str(f_number)
@@ -28,3 +29,40 @@ def excel(name,f_number, diff_percent,split,len_bs,res,sheet):
         ws['J' + str(l)] = '=' + item + '(J' + str(r - 5) + ':J' + str(r - 1) + ')'
         ws['K' + str(l)] = '=' +item+'(K'+str(r-5)+':K' + str(r - 1) + ')'
     wb.save("Input/"+name+".xlsx")
+
+def color(name,sheet_range):
+    excel_app = xw.App(visible=True)
+    wb = excel_app.books.open("Input/" + name + '.xlsx')
+    sheet_i=0
+    for item in wb.sheets:
+        if sheet_i >= sheet_range:
+            break
+        sheet_i+=1
+        min=[1,1,1,1,1,1]
+        max=[0,0,0,0,0,0]
+        row_min = [0, 0, 0, 0, 0, 0]
+        row_max = [0, 0, 0, 0, 0, 0]
+        col = ['D', 'E', 'G', 'H', 'J', 'K']
+        #LastRow = xw.Range('A1').last_cell.row_max
+        for i in range(9,100,8):
+            for c in range(len(col)):
+                print(item[col[c]+str(i)].value)
+                if item[col[c]+str(i)].value==None:
+                    break
+                elif item[col[c]+str(i)].value>max[c]:
+                    row_max[c]=i
+                    max[c]=item[col[c]+str(i)].value
+                elif item[col[c]+str(i)].value<min[c]:
+                    row_min[c]=i
+                    min[c]=item[col[c]+str(i)].value
+        for j in range(len(col)):
+            try:
+                item[col[j] + str(row_min[j])].color = (255, 0, 0)
+                item[col[j]+str(row_max[j])].color = (0,255,0)
+            except:
+                continue
+    wb.save("Input/"+name+'.xlsx')
+    wb.close()
+    excel_app.quit()
+
+
--- test.py
+++ test.py
@@ -8,20 +8,68 @@
 
 
 import os, time
+from output import *
 from fit_data import test_data
 #!!!!!!!!!crossed_arr and len_bs -> Hand
 
 #(len f, percent, accuracy, [] f, data, split sheet)
 #name (9)1-9   0.  weighted-macro-micro [0-8]  smells/equal/none cross  sheet
 
-'''for p in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
-    test_data("_jgraph",9,p, None, [],"equal",5,"Sheet1")
-
 for n in [1,2,3,4,5,6,7,8,9]:
-    test_data("_jgraph",n,0.5, None, [],"equal",5,"Sheet2")'''
+    test_data("_freemind",n,0.6, None, [],"equal",5,"Sheet1")
 
 for arr in [[0],[1],[2],[3],[4],[5],[6],[7],[8]]:
-    test_data("_jgraph",9,0.5, None, arr,"equal",5,"Sheet3")
+    test_data("_freemind",9,0.6, None, arr,"equal",5,"Sheet2")
+
+for n in [1,2,3,4,5,6,7,8,9]:
+    test_data("_freemind",n,0.3, None, [],"equal",5,"Sheet3")
+
+for arr in [[0],[1],[2],[3],[4],[5],[6],[7],[8]]:
+    test_data("_freemind",9,0.3, None, arr,"equal",5,"Sheet4")
+
+color("_freemind",4)
+
+for n in [1,2,3,4,5,6,7,8,9]:
+    test_data("_jgraph",n,0.8, None, [],"equal",5,"Sheet2")
+
+for arr in [[0],[1],[2],[3],[4],[5],[6],[7],[8]]:
+    test_data("_jgraph",9,0.8, None, arr,"equal",5,"Sheet3")
+
+for p in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
+    test_data("_jgraph",9,p, None, [],"equal",5,"Sheet1")
+
+color("_jgraph",3)
+
+for p in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
+    test_data("_junit",9,p, None, [],"equal",5,"Sheet1")
+
+for n in [1,2,3,4,5,6,7,8,9]:
+    test_data("_junit",n,0.6, None, [],"equal",5,"Sheet2")
+
+for arr in [[0],[1],[2],[3],[4],[5],[6],[7],[8]]:
+    test_data("_junit",9,0.6, None, arr,"equal",5,"Sheet3")
+
+for n in [1,2,3,4,5,6,7,8,9]:
+    test_data("_junit",n,0.3, None, [],"equal",5,"Sheet4")
+
+for arr in [[0],[1],[2],[3],[4],[5],[6],[7],[8]]:
+    test_data("_junit",9,0.3, None, arr,"equal",5,"Sheet5")
+
+color("_junit",5)
+
+for n in [1,2,3,4,5,6,7,8,9]:
+    test_data("_jag",n,0.7, None, [],"equal",5,"Sheet1")
+
+for arr in [[0],[1],[2],[3],[4],[5],[6],[7],[8]]:
+    test_data("_jag",9,0.7, None, arr,"equal",5,"Sheet2")
+
+for n in [1,2,3,4,5,6,7,8,9]:
+    test_data("_jag",n,0.6, None, [],"equal",5,"Sheet3")
+
+for arr in [[0],[1],[2],[3],[4],[5],[6],[7],[8]]:
+    test_data("_jag",9,0.6, None, arr,"equal",5,"Sheet4")
+
+color("_jag",4)
 
 #array([recall0,recall1]) array([precision0,precision1]) (ba inke baraks chap shode)
 os.system('mpg123 Input/fuehlen.mp3')
